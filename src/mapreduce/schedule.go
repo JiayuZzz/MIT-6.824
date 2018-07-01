8655ae20ae016a5b713a22ac1c776f3d116630d1
yuwolf <wujy.cs@gmail.com>
2018-07-01 17:40:30 +0800

    parallel workers with fault tolerance (lab1 part3/4)

--- src/mapreduce/schedule.go
+++ src/mapreduce/schedule.go
@@ -1,6 +1,9 @@
 package mapreduce
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
 
 //
 // schedule() starts and waits for all tasks in the given phase (mapPhase
@@ -30,5 +33,21 @@ func schedule(jobName string, mapFiles []string, nReduce int, phase jobPhase, re
 	//
 	// Your code here (Part III, Part IV).
 	//
+	var workGroup sync.WaitGroup
+	for i := 0; i < ntasks; i++ {
+		workGroup.Add(1)
+		workerAddr := <-registerChan
+		args := DoTaskArgs{jobName, mapFiles[i], phase, i, n_other}
+		go func() {
+			defer workGroup.Done()
+			for !call(workerAddr, "Worker.DoTask", args, nil) {
+				workerAddr = <-registerChan
+			}
+			go func() {
+				registerChan <- workerAddr
+			}()
+		}()
+	}
+	workGroup.Wait()
 	fmt.Printf("Schedule: %v done\n", phase)
 }
